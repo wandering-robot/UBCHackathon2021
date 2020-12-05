01bbc632c6b7d4444333faf2166a10ae2563c69d
gacquroff <gavina352@gmail.com>
2020-12-05 09:23:24 -0800

    first app push

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, FlatList } from 'react-native';
+
+export default function App() {
+
+  const [countriesData, setCountriesData] = useState([])
+  function fetchCountriesData() {
+    fetch('https://restcountries.eu/rest/v2/region/africa?fields=name')
+      .then((response) => response.json())
+      .then((json) => setCountriesData(json))
+      .catch((error) => console.error(error))
+  }
+
+  useEffect(()=> {
+    fetchCountriesData();
+  })
+
+  return (
+    <FlatList
+      data={countriesData}
+      contentContainerStyle={styles.container}
+      keyExtractor={item => item.name}
+      renderItem={({item})=> <Text style={styles.text}>{item.name}</Text>}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+  },
+  text: {
+    fontSize: 20,
+    margin: 10
+  },
+});
