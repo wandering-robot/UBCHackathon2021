0769f417b3ada798640e7c4d0ff5a049c8c487b7
gacquroff <gavina352@gmail.com>
2020-12-06 09:41:39 -0800

    successful request from server

--- App.js
+++ App.js
@@ -1,31 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, Button, FlatList, View } from 'react-native';
+import React, {useState, useEffect, Component} from 'react';
+import { render } from 'react-dom';
+import { TouchableOpacity, StyleSheet, Text, Button, FlatList, View, Alert } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
 
-  const [countriesData, setCountriesData] = useState([])
-  function fetchCountriesData() {
-    fetch('https://restcountries.eu/rest/v2/region/africa?fields=name')
-      .then((response) => response.json())
-      .then((json) => setCountriesData(json))
-      .catch((error) => console.error(error))
+
+  // const [countriesData, setCountriesData] = useState([])
+  handlePress = async() => {
+    fetch('https://vast-ocean-93287.herokuapp.com/helloworld')
+      // .then((response) => response.json())
+      // .then((json) => setCountriesData(json))
+      // .catch((error) => console.error(error))
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(responseJson => {
+        console.log(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
-  useEffect(()=> {
-    fetchCountriesData();
-  })
+  // useEffect(()=> {
+  //   fetchCountriesData();
+  // })
 
-  return (
-    <View>
-      <Button title="I am a Button"/>
-      <FlatList
-        data={countriesData}
-        contentContainerStyle={styles.container}
-        keyExtractor={item => item.name}
-        renderItem={({item})=> <Text style={styles.text}>{item.name}</Text>}
-      />
-    </View>
-  );
+  // return (
+  //   <View>
+  //     <Button title="I am a Button!"/>
+
+  //     <FlatList
+  //       data={countriesData}
+  //       contentContainerStyle={styles.container}
+  //       keyExtractor={item => item.server_return}
+  //       renderItem={({item})=> <Text style={styles.text}>{item.name}</Text>}
+  //     />
+  //   </View>
+  // );
+
+  render() {
+    return(
+      <View style={{paddingTop: 50, paddingLeft: 50 }}>
+      <Text> Some other text </Text>
+        <Text> Some other text </Text>
+        <TouchableOpacity onPress={this.handlePress.bind(this)}>
+        <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click me to see the name </Text>
+        </TouchableOpacity>
+      </View> 
+    );
+  }
 }
 
 const styles = StyleSheet.create({
