3bf560a134a285905ea05362ad6cfb49ee378973
gacquroff <gavina352@gmail.com>
2020-12-06 14:29:38 -0800

    fetching

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import React, {useState, useEffect, Component} from 'react';
 import { StyleSheet, Text, Button, FlatList, View, TextInput } from 'react-native';
 import InputField from './components/textdisplay'
 import EmployeeIdeaTextInput from './components/TextInput'
-
-const box_1 = <InputField/>;
+import ViewSolutions from './components/Solutions'
 
 export default class App extends Component {
+
   render() {
     return (
       <View>
         <EmployeeIdeaTextInput/>
+        <ViewSolutions/>
         <InputField/>
         <InputField/>
         <InputField/>
--- components/Solutions.js
+++ components/Solutions.js
@@ -7,18 +7,41 @@ var Solution2 = "We should interview them and find out what they like and do mor
 var Solution3 = "We should lower our prices"
 var Solution4 = "We should tweak foo1 and fix bar3 and that will improve the product"
 
-const arr = [Solution1,Solution2,Solution3,Solution4];
+const arr =[Solution1,Solution4,Solution2,Solution3,Solution4];
 
 const ViewSolutions = () => {
-return (
+    const [arr1, UpdateArray] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://vast-ocean-93287.herokuapp.com/evanCompany/2/4', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        //   // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        // redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // body: JSON.stringify({"userName": text2, "userSolution": text}) // body data type must match "Content-Type" header
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            arr.push(response.Company_Name);
+            UpdateArray(arr);
+        })
+     }, []);
+
+
+    return (
     <View>
         <Text style={{padding:10, fontsize:30}}> 
         {ProblemInfo}
         </Text>
 
-        {arr.map(item => (
-        <Text style={{padding:5, fontsize:12}}>
-        {item.name}</Text>
+        {arr1.map(item => (
+        <Text key={item} style={{padding:5, fontsize:12}}>
+        {item}</Text>
         ))}
 
     </View>
