8bb09c33f06a0b5b13b56799ca5b7e23c59536bf
gacquroff <gavina352@gmail.com>
2020-12-06 12:11:00 -0800

    display multiple text boxes
    - also change src/ to components/

--- App.js
+++ App.js
@@ -1,39 +1,77 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, Button, FlatList, View } from 'react-native';
+import React, {useState, useEffect, Component} from 'react';
+import { StyleSheet, Text, Button, FlatList, View, TextInput } from 'react-native';
+import InputField from './components/textdisplay'
 
-export default function App() {
+const box_1 = <InputField/>;
 
-  const [countriesData, setCountriesData] = useState([])
-  function fetchCountriesData() {
-    fetch('https://restcountries.eu/rest/v2/region/africa?fields=name')
-      .then((response) => response.json())
-      .then((json) => setCountriesData(json))
-      .catch((error) => console.error(error))
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <InputField/>
+        <InputField/>
+        <InputField/>
+        <InputField/>
+      </View>
+      );
   }
-
-  useEffect(()=> {
-    fetchCountriesData();
-  })
-
-  return (
-    <View>
-      <Button title="I am a Button"/>
-      <FlatList
-        data={countriesData}
-        contentContainerStyle={styles.container}
-        keyExtractor={item => item.name}
-        renderItem={({item})=> <Text style={styles.text}>{item.name}</Text>}
-      />
-    </View>
-  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 30,
-  },
-  text: {
-    fontSize: 20,
-    margin: 10
-  },
-});
+
+// // export default function App() {
+//   const UselessTextInput = () => {
+//     const [value, onChangeText] = React.useState("What's your idea?");
+
+//     return (
+//       <TextInput
+//         style={{ height: 400, borderColor: 'gray', borderWidth: 5 }}
+//         onChangeText={text => onChangeText(text)}
+//         value={value}
+//         // console.log(value);
+//       />
+//     );
+// }
+
+// class InputField extends Component {
+//   state = {
+
+//   }
+// }
+
+// export default UselessTextInput;
+// export default function App() {
+
+//   const [countriesData, setCountriesData] = useState([])
+//   function fetchCountriesData() {
+//     fetch('https://restcountries.eu/rest/v2/region/africa?fields=name')
+//       .then((response) => response.json())
+//       .then((json) => setCountriesData(json))
+//       .catch((error) => console.error(error))
+//   }
+
+//   useEffect(()=> {
+//     fetchCountriesData();
+//   })
+
+//   return (
+//     <View>
+//       <Button title="I am a Button"/>
+//       <FlatList
+//         data={countriesData}
+//         contentContainerStyle={styles.container}
+//         keyExtractor={item => item.name}
+//         renderItem={({item})=> <Text style={styles.text}>{item.name}</Text>}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: 30,
+//   },
+//   text: {
+//     fontSize: 20,
+//     margin: 10
+//   },
+// });
