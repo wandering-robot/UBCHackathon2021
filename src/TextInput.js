bc782e59a3e9394f4356c2e1900b8dfbcc2f4415
ericlaycock <ericlaycock44@gmail.com>
2020-12-06 15:32:16 -0500

    TextBranch3

--- src/TextInput.js
+++ src/TextInput.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { Text, TextInput, View } from 'react-native';
 
-
+var userdetails = "Eric L, Marketing"
 const EmployeeIdeaTextInput = () => {
   const [text, setText] = React.useState('');
   return (
-    <View style={{padding: 10}}>
+
+  <View style={{padding: 10}}>
+    {/* USER DETAILS HERE           */}
+      <Text style={{padding:10, fontsize:30}}> 
+        {userdetails}
+      </Text>
+
+    {/* INPUT YOUR IDEA */}
       <TextInput
         style={{height: 40}}
-        placeholder="What's your idea, bitch?"
+        placeholder="What's your idea?"
         onChangeText={text => setText(text)}
         defaultValue={text}
       />
-      <Text style={{padding: 10, fontSize: 42}}>
-        
+    
+    {/* PRINTS TEXT USER ENTERED ABOVE */}
+      <Text style={{padding: 10, fontSize: 22}}>
         {text}
       </Text>
     </View>
